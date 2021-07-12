42c45b453ac6730a0e1db0f30d7953e0f53a88fe
IB Teguh TM <teguh.teja@gmail.com>
2021-07-12 09:33:31 +0800

    ab tests

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -19,6 +19,7 @@ application = get_wsgi_application()
 import inspect
 from ml.registry import MLRegistry
 from ml.income_classifier.random_forest import RandomForestClassifier
+from ml.income_classifier.extra_trees import ExtraTreesClassifier # import ExtraTrees ML algorithm
 
 try:
     registry = MLRegistry() # create ML registry
@@ -34,6 +35,17 @@ try:
                             algorithm_description="Random Forest with simple pre- and post-processing",
                             algorithm_code=inspect.getsource(RandomForestClassifier))
 
+    # Extra Trees classifier
+    et = ExtraTreesClassifier()
+    # add to ML registry
+    registry.add_algorithm(endpoint_name="income_classifier",
+                            algorithm_object=et,
+                            algorithm_name="extra trees",
+                            algorithm_status="testing",
+                            algorithm_version="0.0.1",
+                            owner="Piotr",
+                            algorithm_description="Extra Trees with simple pre- and post-processing",
+                            algorithm_code=inspect.getsource(RandomForestClassifier))
 except Exception as e:
     print("Exception while loading the algorithms to the registry,", str(e))
 
